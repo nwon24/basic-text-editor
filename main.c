6a2fd94da890fe512ae6f22b86ffc4c146bc078b
user <user@localhost.localdomain>
2020-11-01 17:29:18 +1100

    Added quit using 'q'

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 int write_file(char * filename) {
-  char tmp_file_name = "tmp_fp";
+  char tmp_file_name[] = "tmp_fp";
   FILE *fp;
-  system("touch tmp_fp");
   char *in_str = malloc(1);
   int c;
   int i = 0;
@@ -17,16 +16,20 @@ int write_file(char * filename) {
     in_str = realloc(in_str, i+1);
   }
   in_str[i] = '\0';
-  fprintf(tmp_file_name, "%s", in_str);
+  fprintf(fp, "%s", in_str);
   return 0;
 }
 int save_file(char * filename) {
-  char tmp_file_name = "tmp_fp";
+  char tmp_file_name[] = "tmp_fp";
   char buff[100000];
-  fgets(buff,100000,tmp_file_name);
   FILE *fp;
-  fp = fopen(filename, "a");
-  fprintf(fp, "%s", buff);
+  FILE *f;
+  fp = fopen(tmp_file_name, "a");
+  f = fopen(filename, "a");
+  fgets(buff,100000,fp);
+  fprintf(f, "%s", buff);
+  fclose(fp);
+  fclose(f);
   return 0;
 }
 int read_file(char * filename) {
@@ -46,14 +49,17 @@ int read_file(char * filename) {
 int main(int argc, char *argv[]) {
   read_file(argv[1]);
   int b;
-  if ((b = getchar()) == 'a') {
-    if ((b = getchar()) == '\n') {
-      write_file(argv[1]);
+  while ((b = getchar()) != 'q') {
+
+    if ((b = getchar()) == 'a') {
+      if ((b = getchar()) == '\n') {
+        write_file(argv[1]);
+      }
     }
-  }
-  if ((b = getchar()) == 's') {
-    if ((b = getchar()) == '\n') {
-      save_file(argv[1]);
+    if ((b = getchar()) == 's') {
+      if ((b = getchar()) == '\n') {
+        save_file(argv[1]);
+      }
     }
   }
 }
