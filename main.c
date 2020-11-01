eba6d3046089e1e3fa48af592c5ffc0b917c8603
user <user@localhost.localdomain>
2020-11-01 17:08:28 +1100

    fixed syntax errors

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int write_file(char * filename) {
+  char tmp_file_name = "tmp_fp";
   FILE *fp;
   system("touch tmp_fp");
   char *in_str = malloc(1);
   int c;
   int i = 0;
-  fp = fopen(tmp_fp, "a");
+  fp = fopen(tmp_file_name, "a");
   if (!fp) {
     return 1;
   }
@@ -16,12 +17,13 @@ int write_file(char * filename) {
     in_str = realloc(in_str, i+1);
   }
   in_str[i] = '\0';
-  fprintf(tmp_fp, "%s", in_str);
+  fprintf(tmp_file_name, "%s", in_str);
   return 0;
 }
 int save_file(char * filename) {
+  char tmp_file_name = "tmp_fp";
   char buff[100000];
-  fgets(buff,100000,tmp_fp);
+  fgets(buff,100000,tmp_file_name);
   FILE *fp;
   fp = fopen(filename, "a");
   fprintf(fp, "%s", buff);
@@ -44,17 +46,15 @@ int read_file(char * filename) {
 int main(int argc, char *argv[]) {
   read_file(argv[1]);
   int b;
-  while ((b = getchar()) != "q")  {
-    
-    if ((b = getchar()) == 'a') {
-      if ((b = getchar()) == '\n') {
-	write_file(argv[1]);
-      }
+  if ((b = getchar()) == 'a') {
+    if ((b = getchar()) == '\n') {
+      write_file(argv[1]);
     }
-    if ((b = getchar()) == 's') {
-      if ((b = getchar()) == '\n') {
-	save_file(argv[1]);
   }
-       
+  if ((b = getchar()) == 's') {
+    if ((b = getchar()) == '\n') {
+      save_file(argv[1]);
+    }
+  }
 }
 
