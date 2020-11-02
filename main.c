c5df472ee3883a13ce87722925c9f426895e26c8
nwon24 <nwonb@schools.vic.edu.au>
2020-11-03 09:16:39 +1100

    Fixed what happens when called without argument.

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 
 int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    system("clear");
+    printf("Must supply filename as argument! (Will create file if doesn't exist).\n");
+    return 0;
+  }
   read_file(argv[1]);
   copy_file(argv[1]);
   command_mode(argv[1]);
