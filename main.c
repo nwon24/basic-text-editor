bdc1c1d294068f12a1a239bc4b7fc656b94fa655
nwon24 <nwonb@schools.vic.edu.au>
2020-11-02 20:13:51 +1100

    updated main files

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+
+int main(int argc, char *argv[]) {
+  read_file(argv[1]);
+  command_mode(argv[1]);
+       
+}
+
---
+++ main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define WRITE_FILE_COMMAND 'a'
+#define SAVE_FILE_COMMAND 's'
+#define QUIT_PROGRAM_COMMAND 'q'
+#include </usr/include/curses.h>
+#include </usr/include/ncurses.h>
+
+int read_file(char * filename);
+int write_file(char * filename);
+int save_file(char * filename);
+int command_mode(char * filename);
