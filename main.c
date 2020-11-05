0bbae89583c08cb825c040f9fe0b67cb6879fb4d
nwon24 <nwonb@schools.vic.edu.au>
2020-11-05 21:35:41 +1100

    added copy file to beginning of main so that append function works correctly without overwriting old file

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 
 
 int main(int argc, char *argv[]) {
-  char *buffer[3];
-  char ch;
   if (argc == 1) {
     system("clear");
     printf("Must supply filename as argument! (Will create file if doesn't exist).\n");
     return 0;
   }
+  copy_file(argv[1]);
   read_file(argv[1]);
   command_mode(argv[1]);
   
