cb6d0438c9c6cf6efa200660a99d88509efc0d93
nwon24 <60217282+nwon24@users.noreply.github.com>
2020-11-13 07:59:11 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 
-int main (int argc, char *argv[]) {
-  if (argc == 1) {
-    system ("clear");
-    printf ("Must supply filename as argument! (Will create file if doesn't exist).\n");
-    return 0;
-  }
-  copy_file (argv[1], "tmp");
-  read_file (argv[1]);
-  command_mode (argv[1]);
-  remove("tmp");
-  remove("tmp2");
+int main (int argc, char *argv[])
+{
+        if (argc == 1) {
+                system ("clear");
+                printf ("Must supply filename as argument! (Will create file if doesn't exist).\n");
+                return 0;
+        }
+        copy_file (argv[1], "tmp");
+        read_file (argv[1]);
+        command_mode (argv[1]);
+        remove("tmp");
+        remove("tmp2");
 
 }
-
