94849144c9b1fda9398c382b5398a10d87fac8cd
Juicymangoes123 <53684212+Juicymangoes123@users.noreply.github.com>
2020-11-14 11:20:56 +1100

    Stable version

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 
-int main(int argc, char *argv[]) {
-  int ch;
-  if (argc == 1) {
-    system("clear");
-    printf("Must supply filename as argument! (Will create file if doesn't exist).\n");
-    return 0;
-  }
-  read_file(argv[1]);
-  command_mode(argv[1]); 
+int main(int argc, char *argv[]) 
+{
+	int ch;
+  	if (argc == 1) {
+    		system("clear");
+    		printf("Must supply filename as argument! (Will create file if doesn't exist).\n");
+    		return 0;
+  	}
+  	read_file(argv[1]);
+  	command_mode(argv[1]); 
 }
 
